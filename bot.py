3d93dac9a1fd5121b090d275a2942296614499a8
Ulysses681 <ulyssesalgado1@yahoo.com>
2026-03-01 19:26:39 -0600

    Change command prefix from '$' to '^'

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="$", intents=intents, help_command=None)
+bot = commands.Bot(command_prefix="^", intents=intents, help_command=None)
 
 # ================= DATABASE =================
 conn = sqlite3.connect("database.db")
